79c57f8dfa204fe42af35e7f78dd34bda7dda6e4
Franco Taboada <107222570+Taboadafranco02@users.noreply.github.com>
2023-05-08 15:21:52 -0300

    Update FrmVentanaCatalago.cs

--- Tp2 Programacion/FrmVentanaCatalago.cs
+++ Tp2 Programacion/FrmVentanaCatalago.cs
@@ -218,7 +218,7 @@ namespace Tp2_Programacion
             }
             catch (Exception ex)
             {
-                MessageBox.Show("caca");
+                
                 MessageBox.Show(ex.ToString());
                 
             }
